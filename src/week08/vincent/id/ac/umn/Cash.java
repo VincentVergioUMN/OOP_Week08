41786989018bbb2f88a792195e004187b1735244
VincentVergioUMN <vincent.vergio@student.umn.ac.id>
2025-10-29 11:17:31 +0700

    Cash class

---
+++ src/week08/vincent/id/ac/umn/Cash.java
@@ -0,0 +1,25 @@
+package week08.vincent.id.ac.umn;
+
+public class Cash extends Payment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public Cash(Item item) {
+		super(item);
+	}
+	
+	public int pay() {
+		if(isPaidOff) {
+			return 0;
+		}
+		isPaidOff = true;
+		return this.item.getPrice();
+	}
+	
+	public String getClassName() {
+		return "CASH";
+	}
+	
+}
